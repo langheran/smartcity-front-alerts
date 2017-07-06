16a7882ddd13f29cb03acb540c4d452768d5f5a4
FlavioBaez <flaviocesar_baez@hotmail.com>
2017-07-05 21:57:34 -0500

    ocultar sidenav

--- src/app/template/top-menu/top-menu.component.ts
+++ src/app/template/top-menu/top-menu.component.ts
@@ -10,9 +10,15 @@ export class TopMenuComponent implements OnInit {
   constructor() {
   }
   ngOnInit() {
+    
   }
+
+   
+
   toggleSidebar() {
+    
     const dom: any = document.querySelector('body');
     dom.classList.toggle('push-right');
   }
+
 }
